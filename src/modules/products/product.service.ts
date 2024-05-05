efb27de9e5c3b6f2b5857f3c9a2239d5ffe5430d
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-05 17:37:34 +0700

    feat: delete product by id

--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -36,8 +36,13 @@ export class ProductService{
         return this.products[index];
     }
 
-    deleteProduct(): string {
-        return 'DELETE product';
+    deleteProduct(id: number): boolean {
+        const index = this.products.findIndex(item => item.id === Number(id));
+        if (index !== -1) {
+            this.products.splice(index, 1);
+            return true;
+        }
+        return false;
     }
 
 }; 
