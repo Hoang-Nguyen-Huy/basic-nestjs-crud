6c7ebf22f9bb4ff2e85641ff2a255ecefd51e9a9
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-05-11 23:13:44 +0700

    feat: encrypting password before saving to database

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UsersDto } from 'src/dto/user.dto';
 import { UsersEntity } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -14,7 +15,11 @@ export class UsersService {
     /*
     [POST]: Register
     */
-    createUser(userDto: UsersDto): Promise<UsersEntity> {
+    async createUser(userDto: UsersDto): Promise<UsersEntity> {
+        const saltOrRounds = 10;
+        const password = 'random_password';
+        const hash = await bcrypt.hash(password, saltOrRounds);
+        userDto.password = hash;
         return this.userRepository.save(userDto);
     }
 }
